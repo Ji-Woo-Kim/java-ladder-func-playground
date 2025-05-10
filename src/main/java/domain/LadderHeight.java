8c728c72f174cb1d484092af26f5eff007106e3f
Jiwoo Kim <ziuoo0424@gmail.com>
2025-05-11 07:36:19 +0900

    refactor: 네이밍 변경

--- src/main/java/domain/LadderHeight.java
+++ src/main/java/domain/LadderHeight.java
@@ -1,10 +1,10 @@
 package domain;
 
-public record Height(int value) {
+public record LadderHeight(int value) {
 
     private static final int MIN_HEIGHT_VALUE = 1;
 
-    public Height {
+    public LadderHeight {
         if (value < MIN_HEIGHT_VALUE) {
             throw new IllegalArgumentException("사다리의 높이는 1 이상이어야 합니다.");
         }
