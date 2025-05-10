37f13b457bd0e5262af7a8353cd662301d47e5ee
Jiwoo Kim <ziuoo0424@gmail.com>
2025-05-11 07:35:35 +0900

    refactor: Enum 적용

--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -1,24 +1,33 @@
 package domain;
 
-public class Point {
+public enum Point {
 
-    private boolean connectedToRight;
-    private boolean connectedToLeft;
+    // 왼쪽으로 연결됨
+    CONNECTED_LEFT(-1),
+    // 오른쪽으로 연결됨
+    CONNECTED_RIGHT(1),
+    // 양쪽 아무 곳에도 연결되지 않음
+    NOT_CONNECTED(0);
 
-    public Point() {
-        this.connectedToRight = false;
-        this.connectedToLeft = false;
+    private final int offset;
+
+    Point(int offset) {
+        this.offset = offset;
     }
 
-    public boolean isConnectedToRight() {
-        return connectedToRight;
+    public int moveFrom(int currentPosition) {
+        return currentPosition + offset;
     }
 
-    public void connectToRight() {
-        this.connectedToRight = true;
+    public boolean isConnectedLeft() {
+        return this == CONNECTED_LEFT;
     }
 
-    public void connectToLeft() {
-        this.connectedToLeft = true;
+    public boolean isConnectedRight() {
+        return this == CONNECTED_RIGHT;
+    }
+
+    public boolean isNotConnected() {
+        return this == NOT_CONNECTED;
     }
 }
